01657f97cd0dd7f39143ddf1151077380b7305da
Thomas Zwickl <thomas.zwickl@motius.de>
2019-08-30 22:43:37 +0200

    Revert "Remove experiment files."
    This reverts commit 3741c278a98c134740c1fdf5a95a9fa8ac2b31c6.

---
+++ experiments/experiment1.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+START_RATE=0.1
+STEP_RATE=0.1
+MAX_RATE=0.1
+TIME_LIMIT=10
+
+ITRS_V=(0x000 0x008 0x010 0x028 0x0C8 0x190 0x320 0x4B0 0x640 0x7D0 0x960 0xAF0 0xC80 0xE10 0xFA7)
+ITRS_S=(0 2 4 10 50 100 200 300 400 500 600 700 800 900 1000)
+
+for i in "${!ITRS_V[@]}"; do
+  ./run.sh experiment1_1 ${START_RATE} ${STEP_RATE} ${MAX_RATE} ${TIME_LIMIT} ${ITRS_V[$i]} "Poisson"
+  mkdir ${ITRS_S[$i]}
+  mv *.csv ${ITRS_S[$i]}
+done
---
+++ experiments/l2-load-latency.lua
@@ -0,0 +1,62 @@
+local mg     = require "moongen"
+local memory = require "memory"
+local device = require "device"
+local ts     = require "timestamping"
+local stats  = require "stats"
+local timer  = require "timer"
+
+local PKT_SIZE	= 60
+local ETH_DST	= "11:12:13:14:15:16"
+
+local function getRstFile(...)
+    local args = { ... }
+    for i, v in ipairs(args) do
+        result, count = string.gsub(v, "%-%-result%=", "")
+        if (count == 1) then
+            return i, result
+        end
+    end
+    return nil, nil
+end
+
+function configure(parser)
+    parser:description("Generates directional CBR traffic with hardware rate control and measure latencies.")
+    parser:argument("dev1", "Device to transmit from."):convert(tonumber)
+    parser:argument("dev2", "Device to receive from."):convert(tonumber)
+    parser:option("-r --rate", "Transmit rate in mp/s."):default("1"):convert(tonumber)
+    parser:option("-t --timeLimit", "Time limit in seconds."):default("10"):convert(tonumber)
+end
+
+function master(args)
+    local dev1 = device.config({port = args.dev1, rxQueues = 1, txQueues = 1})
+    local dev2 = device.config({port = args.dev2, rxQueues = 1, txQueues = 1})
+    device.waitForLinks()
+    dev1:getTxQueue(0):setRate(args.rate * (PKT_SIZE + 4) * 8)
+    mg.startTask("loadSlave", dev1:getTxQueue(0), args.timeLimit)
+    stats.startStatsTask{dev1, dev2}
+    mg.waitForTasks()
+end
+
+function loadSlave(queue, timeLimit)
+    local mem = memory.createMemPool(function(buf)
+        buf:getEthernetPacket():fill{
+            ethSrc = txDev,
+            ethDst = ETH_DST,
+            ethType = 0x1234
+        }
+    end)
+    local bufs = mem:bufArray()
+    local timeLimit = timer:new(timeLimit)
+    local seq = 1
+    while mg.running() and not timeLimit:expired() do
+        bufs:alloc(PKT_SIZE)
+        for i, buf in ipairs(bufs) do
+            local pkt = buf:getUdpPacket()
+            pkt.payload.uint64[1] = seq
+            seq = seq + 1
+        end
+        queue:send(bufs)
+    end
+    queue:stop()
+    mg:stop()
+end
